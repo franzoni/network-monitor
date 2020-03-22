aea2c9c5686d6f37b0be91aa6ec022d791fa3dcb
Giovanni Franzoni <giovanni.franzoni@cern.ch>
2020-03-22 16:27:09 +0100

    formatting done; NEEDS CLEANUP; add option for more domains/targets

--- ping_monit.py
+++ ping_monit.py
@@ -2,20 +2,23 @@
 import subprocess
 from utils import *
 
-host = "www.google.com"
+targets = ["google.com"]
+target = targets[0]
 
+host = get_host()
 count=0
+
+# ADD A HEADER LINE TO THE .DAT file
 
 while True:
 	count+=1
 	
 	ping = subprocess.Popen(
-	    ["ping", "-c", "10", "-i", "0.1", host],
+	    ["ping", "-c", "10", "-i", "0.1", target],
 	    stdout = subprocess.PIPE,
 	    stderr = subprocess.PIPE
 	)
 	p_out, p_error = ping.communicate()
-
 
 	date = subprocess.Popen(
 	    ["date", "+%F %T"],
@@ -24,19 +27,26 @@ while True:
 	)
 	d_out, d_error = date.communicate()
 
-	print '\n\ntimeT  : %s'%d_out
-	print 'timeE : %s\n'%d_error
-	print 'ping :\n %s'%p_out
-	print 'ping parsed:\n %s'%p_out
-	print 'pingE:\n %s'%p_error
+	# print '\n\ntimeT  : %s'%d_out
+	#print 'timeE : %s\n'%d_error
+	#print 'ping :\n %s'%p_out
+	#print 'ping :\n %s'%select_ping_summary_line(p_out)
+        ping_summary = summary_from_full_report(p_out)
+	# print 'ping :\n %d %d %f'%ping_summary
+	#print 'ping parsed:\n %d %d %s'%(unpack_ping(p_out))
+	#print 'pingE:\n %s'%p_error
 
-	fname = 'pings-'+d_out.split()[0]+'.dat'
+        # data_point = d_out.strip('\n') + ' ' + targer + ' ' + str(ping_summary[0]) + ' ' + str(ping_summary[1]) + ' ' + str(ping_summary[2])
+        data_point = format_data_point(d_out, host, target, ping_summary)
+        # print 'give you a debug show'
+        print data_point
+	fname = 'pings-DEV-'+d_out.split()[0]+'.dat'
 	bufsize=9
 	f = open(fname, 'a', buffering=bufsize) 	
-	f.write('\n\ntimeT : %s'%d_out)
-	f.write('timeE : %s\n'%d_error)
-	f.write('ping :\n %s'%p_out)
-	f.write('pingE:\n %s'%p_error)
+	#f.write('\n\ntimeT : %s'%d_out)
+	#f.write('timeE : %s\n'%d_error)
+	#f.write('ping :\n %s'%p_out)
+	f.write(data_point)
 
 	if count%10==0:
 		f.flush()
--- utils.py
+++ utils.py
@@ -1,2 +1,34 @@
-def unpack_ping(the_ping):
-        return the_ping.split()[0],the_ping.split()[3],the_ping.split()[6],
+def summary_from_full_report(ping_report):
+        relevant_line = select_ping_summary_line(ping_report)
+        return unpack_summary_line(relevant_line)
+        
+def unpack_summary_line(the_line):
+        return int(the_line.split()[0]), int(the_line.split()[3]), float(the_line.split()[6].strip('%'))
+
+def select_ping_summary_line(ping_report):
+        split_report = ping_report.split('\n')
+        relevant_line = [u for u in split_report if 'packets transmitted' in u]
+        return relevant_line[0]
+
+def format_data_point(d_out, host, target, ping_summary):
+        return d_out.strip('\n') + ' ' + host + ' ' + target + ' ' + str(ping_summary[0]) \
+                + ' ' + str(ping_summary[1]) + ' ' + str(ping_summary[2]) + '\n'
+
+def encode_host(hostname_out):
+        if 'Giovannis-MacBook-Pro-619.local' in hostname_out:
+                return 'gf_mac'
+        elif  'Giovannis-MacBook-Pro-619.local' in hostname_out:
+                return 'ad_mac'
+        else:
+                return 'unkn'
+
+import subprocess        
+def get_host():
+        hostname = subprocess.Popen(
+	    ["hostname",],
+	    stdout = subprocess.PIPE,
+	    stderr = subprocess.PIPE
+	)
+	p_out, p_error = hostname.communicate()
+        return encode_host(p_out)
+        
